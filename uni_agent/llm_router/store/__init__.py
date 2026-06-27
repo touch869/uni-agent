3689054c9db4eb0ba1cb8b4b1324238a700f65d3
yyyyrf <yourifan@outlook.com>
2026-06-27 16:55:45 +0800

    mv data write to collector

--- uni_agent/llm_router/store/__init__.py
+++ uni_agent/llm_router/store/__init__.py
@@ -1,11 +1,11 @@
-"""Metric stores and unified query provider for routing decisions."""
+"""Metric stores and unified data access layer for routing decisions."""
 
+from uni_agent.llm_router.store.data_store import DataStore
 from uni_agent.llm_router.store.kv_cache_store import KVCacheStore
 from uni_agent.llm_router.store.metrics_store import MetricsStore
-from uni_agent.llm_router.store.provider import StoreProvider
 
 __all__ = [
+    "DataStore",
     "KVCacheStore",
     "MetricsStore",
-    "StoreProvider",
 ]
